aaf5dd140b68b8a7f87c78cac76ca2e33699fbdd
Viii <vihirmujahid8@gmail.com>
2023-12-04 13:55:50 +0700

    PUSH

---
+++ sipus/app/Models/Mahasiswa.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Mahasiswa extends Model
+{
+    use HasFactory;
+
+    //jika nama table berbeda
+    protected $table = 'mahasiswas';
+
+    //untuk mengatur kolom yang boleh di isi saat insert
+
+    protected $fillable = ['npm', 'nama', 'tempat_lahir', 'tanggal_lahir'];
+
+    protected $guarded = [];
+}
